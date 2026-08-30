fbee70f43120bc5a9db10f9e47830a4c376d5361
NotNoss <noss@itsnoss.com>
2026-08-30 07:52:06 -0500

    Added install script and .stow-local-ignore file

---
+++ install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+packages=(
+  ghostty
+  hyprland
+  hyprpicker
+  hyprlock
+  awww
+  matugen
+  neovim
+  yazi
+  ripgrep
+  serpl
+  lazygit
+  npm
+  tree-sitter
+  tree-sitter-cli
+  quickshell
+  ttf-firacode-nerd
+  slurp
+  grim
+  hyprpicker
+  polkit
+  playerctl
+  tmux
+  zoxide
+  zsh
+  yarn
+  stow
+  libfido2
+)
+
+aur_packages=(
+  oh-my-posh
+  ttf-material-symbols-variable-git
+)
+
+if [[ $EUID -eq 0 ]]; then
+  echo "Please run the script as a normal user. The install script will call sudo when needed." >&2
+  exit 1
+fi
+
+echo "Synchronizing package databases..."
+yay -Sy
+
+echo "Installing packages..."
+sudo pacman -S --needed --noconfirm "${packages[@]}"
+yay -S --needed --noconfirm "${aur_packages[@]}"
+
+echo "Creating dotfiles directories..."
+mkdir -p "$HOME/.config/anarchy/.cache/"
+
+echo "Cloning github repo..."
+git clone "git@github.com:NotNoss/Anarchy-Dots.git" "$HOME/.config/anarchy/dots"
+
+echo "Creating symlinks..."
+stow -t "$HOME" -d "$HOME/.config/anarchy/dots/"
+
+echo "Done. Everything should be setup now"
