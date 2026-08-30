267fe25ac81cf09924afe55739effd64615c6d18
NotNoss <noss@itsnoss.com>
2026-08-30 10:42:33 -0500

    Added sddm

--- install.sh
+++ install.sh
@@ -27,11 +27,13 @@ packages=(
   zsh
   yarn
   stow
+  sddm
 )
 
 aur_packages=(
   oh-my-posh
   ttf-material-symbols-variable-git
+  sddm-silent-theme
 )
 
 if [[ $EUID -eq 0 ]]; then
@@ -53,7 +55,14 @@ echo "Cloning github repo..."
 git clone "https://github.com/NotNoss/Anarchy-Dots.git" "$HOME/.config/anarchy/dots"
 
 echo "Creating symlinks..."
-cd "$HOME/.config/anarchy/dots"
+cd "$HOME/.config/anarchy/dots/home/"
 stow -t "$HOME" .
+
+cd "$HOME/.config/anarchy/dots/etc/"
+sudo stow -t "/etc/" .
+/usr/share/sddm/themes/silent/change_avatar.sh "$USER" "$HOME/.config/anarchy/dots/Anarchy-Logo.png"
+
+echo "Enabling SDDM..."
+sudo systemctl enable --now sddm
 
 echo "Done. Everything should be setup now"
